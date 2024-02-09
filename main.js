35dd0859dc8cb3ce643fa6cb6bdbc614c0f9953a
AlfonsoRReyes <alfonso@analogcomputation.com>
2024-02-09 17:33:32 -0600

    use variable "addTitle" in the render() function, and add "Table of Contents" if true.

--- main.js
+++ main.js
@@ -113,7 +113,7 @@ class Renderer extends MarkdownRenderChild {
       if (options.debugInConsole) debug('Markdown', markdown)
 
       this.element.empty()
-      markdown = '## Table of Contents\n' + markdown
+      if (options.addTitle)  markdown = '## Table of Contents\n' + markdown
       MarkdownRenderer.renderMarkdown(markdown, this.element, this.sourcePath, this)
     } catch(error) {
       const readableError = `_💥 Could not render table of contents (${error.message})_`
